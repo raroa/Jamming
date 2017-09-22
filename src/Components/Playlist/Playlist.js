dcb1bd4f871e13c2115657acd428f8024f27f9cb
raroa <darkkcity@yahoo.com>
2017-09-22 17:13:16 +0300

    this binding modified

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,7 @@ import { TrackList } from '../TrackList/TrackList';
  export class Playlist extends React.Component {
     constructor(props) {
         super(props);
-        this.onNameChange = this.onNameChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
     }
 
     handleNameChange(e) {
